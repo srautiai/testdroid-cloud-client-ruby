ca933b579c16d472434c8c19b0b1fc0ca0520328
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2012-10-23 13:30:20 +0300

    Removed script

--- lib/testdroid-cloud.rb
+++ lib/testdroid-cloud.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/ruby
 
-
+require 'json'
+require 'rest-client'
 require 'testdroid-cloud'
+require 'cloud_resource'
+require 'cloud_list_resource'
+require 'projects/projects'
+require 'test_run'
+require 'config'
+require 'testdroid_client'
+require 'user'
 
+require 'test_run'
+require 'test_run'
 
-
-if __FILE__ == $0    
-	begin
-		
-	end
-end
