0028da1654fb20b5101ed6fd3159f25e58c19e70
Sakari Rautiainen <sakari.rautiainen@bitbar.com>
2012-10-23 13:50:09 +0300

    Fixed spec

--- spec/testdroid_cloud_spec.rb
+++ spec/testdroid_cloud_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
-require 'testdroid-cloud'
 
-describe TestdroidCloud do
+describe Testdroid::Cloud do
   it "should have a VERSION constant" do
     subject.const_get('VERSION').should_not be_empty
   end
